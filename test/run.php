c27b1497a1acfa9a3ad1a982795cf648f95e0050
shabbyrobe <code@shabbyrobe.org>
2013-04-03 15:35:12 +1100

    updated test to use submodule

--- test/run.php
+++ test/run.php
@@ -1,30 +1,28 @@
 <?php
-$basePath = __DIR__;
-require $basePath.'/../src/docopt.php';
+$basePath = __DIR__.'/../';
+$testPath = __DIR__;
+require $basePath.'/src/docopt.php';
 require_once 'PHPUnit/Autoload.php';
 
-require $basePath.'/lib/PythonPortedTest.php';
-require $basePath.'/lib/LanguageAgnosticTest.php';
+require $testPath.'/lib/PythonPortedTest.php';
+require $testPath.'/lib/LanguageAgnosticTest.php';
 
 $options = array(
-    'py'=>getenv('DOCOPT_PYTHON_PATH'),
     'filter'=>null,
 );
 $options = array_merge(
     $options,
-    getopt('', array('py:', 'filter:'))
+    getopt('', array('filter:'))
 );
 
-if (!$options['py']) {
-    die(
-        "Please ensure the --py option or the DOCOPT_PYTHON_PATH environment\n".
-        "variable point to the path of the python port"
-    );
+$pyTestFile = $basePath.'/py/testcases.docopt';
+if (!file_exists($pyTestFile)) {
+    die("Please ensure you have loaded the git submodules\n");
 }
 
 $suite = new PHPUnit_Framework_TestSuite();
 $suite->addTest(new PHPUnit_Framework_TestSuite('Docopt\Test\PythonPortedTest'));
-$suite->addTest(Docopt\Test\LanguageAgnosticTest::createSuite($options['py'].'/testcases.docopt'));
+$suite->addTest(Docopt\Test\LanguageAgnosticTest::createSuite($pyTestFile));
 
 $runner = new PHPUnit_TextUI_TestRunner();
 $runner->doRun($suite, array(
